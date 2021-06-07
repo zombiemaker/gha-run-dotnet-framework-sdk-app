ef9ed72cff8b8a8c8e508c85527c55e03968cb3e
zombiemaker <8773975+zombiemaker@users.noreply.github.com>
2021-06-07 16:18:57 -0400

    Debugging

--- scripts/gha-pshell-module.psm1
+++ scripts/gha-pshell-module.psm1
@@ -175,7 +175,7 @@ function Invoke-ContainerizedDotnetSdkCommand {
                 #& docker run --rm -v $PowerShellHostWorkingDirectory:c:/Users/ContainerAdministrator/Documents -w "c:\Users\ContainerAdministrator\Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString
                 & echo "PowerShell invocation operator call"
                 & echo "type=bind,source=$PowerShellHostWorkingDirectory,target=c:/users/containeradministrator/documents"
-                & docker run --rm --mount type=bind,source="$PowerShellHostWorkingDirectory",target="c:/users/containeradministrator/documents" -w "c:\Users\ContainerAdministrator\Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 dir
+                & docker run --rm --mount type=bind,source="$PowerShellHostWorkingDirectory",target="c:/users/containeradministrator/documents" -w "c:\Users\ContainerAdministrator\Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 powershell -Command dir
             }
             
             continue
