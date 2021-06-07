d849f0f169a29267c2a35d3e832d5d11a16aea71
zombiemaker <8773975+zombiemaker@users.noreply.github.com>
2021-06-07 13:59:06 -0400

    Fixing docker invocation

--- scripts/gha-pshell-module.psm1
+++ scripts/gha-pshell-module.psm1
@@ -173,7 +173,9 @@ function Invoke-ContainerizedDotnetSdkCommand {
                 
                 # Get error: docker: Error response from daemon: Unrecognised volume spec: invalid volume specification: '/Users/ContainerAdministrator/Documents'
                 #& docker run --rm -v $PowerShellHostWorkingDirectory:c:/Users/ContainerAdministrator/Documents -w "c:\Users\ContainerAdministrator\Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString
-                & docker run --rm -v "$PowerShellHostWorkingDirectory:c:/users/containeradministrator/documents" -w "c:\Users\ContainerAdministrator\Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString
+                & echo PowerShell invocation operator call
+                & echo $PowerShellHostWorkingDirectory
+                & docker run --rm -v $PowerShellHostWorkingDirectory:c:/users/containeradministrator/documents" -w "c:\Users\ContainerAdministrator\Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString
             }
             
             continue
