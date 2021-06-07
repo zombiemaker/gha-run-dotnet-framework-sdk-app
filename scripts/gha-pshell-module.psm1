269475beae20cdc974621ce71af5860e5d03032c
zombiemaker <8773975+zombiemaker@users.noreply.github.com>
2021-06-07 10:26:45 -0400

    Trying base64

--- scripts/gha-pshell-module.psm1
+++ scripts/gha-pshell-module.psm1
@@ -138,11 +138,13 @@ function Invoke-ContainerizedDotnetSdkCommand {
             if ($PowerShellHostWorkingDirectory -eq $null) {
                 write-host "docker run --rm mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString"
                 # Had issues with quotations in the $CommandString when executing docker program
-                Invoke-Expression "docker run --rm mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString"
+                #Invoke-Expression "docker run --rm mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString"
+                & docker run --rm mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString
             } else {
                 write-host "docker run --rm -v '$($PowerShellHostWorkingDirectory):c:/Users/ContainerAdministrator/Documents' -w 'c:/Users/ContainerAdministrator/Documents' mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString"
                 # Had issues with quotations in the $CommandString when executing docker program
-                Invoke-Expression "docker run --rm -v '$($PowerShellHostWorkingDirectory):c:/Users/ContainerAdministrator/Documents' -w 'c:/Users/ContainerAdministrator/Documents' mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString"
+                #Invoke-Expression "docker run --rm -v '$($PowerShellHostWorkingDirectory):c:/Users/ContainerAdministrator/Documents' -w 'c:/Users/ContainerAdministrator/Documents' mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString"
+                & docker run --rm -v "$PowerShellHostWorkingDirectory:c:/Users/ContainerAdministrator/Documents" -w "c:/Users/ContainerAdministrator/Documents" mcr.microsoft.com/dotnet/framework/sdk:4.8 $CommandString
             }
             
             continue
@@ -155,11 +157,13 @@ function Invoke-ContainerizedDotnetSdkCommand {
             if ($PowerShellHostWorkingDirectory -eq $null) {
                 write-host "docker run --rm mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString"
                 # Had issues with quotations in the $CommandString when executing docker program
-                Invoke-Expression "docker run --rm mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString"
+                #Invoke-Expression "docker run --rm mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString"
+                & docker run --rm mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString
             } else {
                 write-host "docker run --rm -v '$($PowerShellHostWorkingDirectory):c:/Users/ContainerAdministrator/Documents' -w 'c:/Users/ContainerAdministrator/Documents' mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString"
                 # Had issues with quotations in the $CommandString when executing docker program
-                Invoke-Expression "docker run --rm -v '$($PowerShellHostWorkingDirectory):c:/Users/ContainerAdministrator/Documents' -w 'c:/Users/ContainerAdministrator/Documents' mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString"
+                #Invoke-Expression "docker run --rm -v '$($PowerShellHostWorkingDirectory):c:/Users/ContainerAdministrator/Documents' -w 'c:/Users/ContainerAdministrator/Documents' mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString"
+                & docker run --rm -v "$PowerShellHostWorkingDirectory:c:/Users/ContainerAdministrator/Documents" -w "c:/Users/ContainerAdministrator/Documents" mcr.microsoft.com/dotnet/framework/sdk:3.5 $CommandString
             }
             continue
         }
