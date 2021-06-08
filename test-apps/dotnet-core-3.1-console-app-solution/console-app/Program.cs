37719fb06d300f866cc72d4e9f7f82a7b21d1e04
zombiemaker <8773975+zombiemaker@users.noreply.github.com>
2021-06-08 09:59:20 -0400

    Changed test workflow and data

---
+++ test-apps/dotnet-core-3.1-console-app-solution/console-app/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
